22d7f70034f610479a4c2a536b49a8f901956b79
GwendalLaurent <gwendal.laurent@student.uclouvain.be>
2023-11-03 10:52:48 +0100

    mac_layer documentation

--- src/mac_layer.erl
+++ src/mac_layer.erl
@@ -4,6 +4,9 @@
 -include("mac_frame.hrl").
 -include("gen_mac_layer.hrl").
 
+%% This module implmements the gen_mac_layer behaviour.
+%% This file can be taken as an example when you want to build a mock-up for the MAC layer
+
 %%% gen_mac_layer callbacks
 -export([init/1]).
 -export([tx/4]).
@@ -14,6 +17,15 @@
 
 %--- mac_layer_behaviour callback functions ---------------------------------------------
 
+% @doc Initialize the MAC layer
+% 
+% By default, it activate fthe frame filtering with auto-acknowledgment
+% It also set the frame waiting time out to MACACKWAITDURATION. However, the timeout isn't activated
+% If you want to use it, it has to be activated later on.
+% 
+% Params is a map that can contain different elements:
+% * `phy_layer': specifying which driver it should use. By default it uses the driver of the pmod_uwb
+% @end
 -spec init(Params::term()) -> State :: term().
 init(Params) ->
     PhyModule = case Params of
@@ -25,7 +37,12 @@ init(Params) ->
     PhyModule:write(sys_cfg, #{ffab => 1, ffad => 1, ffaa => 1, ffam => 1, ffen => 1, autoack => 1}),
     #{phy_layer => PhyModule, retries => 0}. 
 
-
+% @doc transmits a frame using the physical layer
+% 
+% If the frame control specify that an ACK is requested (AR=1), the function will wait for the ACK before returning
+% Moreover, if the ACK doesn't arrive before MACACKWAITDURATION, a retransmission is done
+% After MACMAXFRAMERETRIES retransmission, if the ACK doesn't arrive, the function returns the error `no_ack'
+% @end
 tx(#{retries := ?MACMAXFRAMERETRIES} = State, Framecontrol, _, _) ->
     case Framecontrol#frame_control.frame_type of
         ?FTYPE_DATA -> {error, State#{retries => 0}, no_ack}; % This error should be triggered in a direct transmission 
@@ -49,17 +66,31 @@ tx(#{phy_layer := PhyModule} = State, FrameControl, MacHeader, Payload) ->
         Error -> {error, State, Error}
     end.
 
+% @doc Performs the reception of a single packet
+% 
+% @param State: The state of the module
+% @param RxEnabled: Specifies if the reception has been enabled previously. In most cases this value should be `false'
+% @end
 rx(#{phy_layer := PhyModule} = State, RxEnabled) ->
     case PhyModule:reception(RxEnabled) of
         {_Length, Frame} -> {ok, State, mac_frame:decode(Frame)};
         Err -> {error, State, Err}
     end.
 
+% @doc Turns on the continuous reception
+% 
+% When a packet is received, it will call the function specified in the parameter `Callback'
+% If the received packet is a data packet requesting an ACL and the autoack is turned on (by default)
+% the ACK is handled by the hardware before calling the callback
+% 
+% @end
 rx_on(#{phy_layer := PhyModule} = State, Callback) ->
     PhyModule:write(sys_cfg, #{rxwtoe => 0}),
     LoopPid = spawn_link(fun() -> rx_loop(State, Callback) end), 
     {ok, State#{loop_pid => LoopPid}}.
 
+% @doc Turns off the continuous reception
+% @end
 rx_off(#{phy_layer := PhyModule} = State) ->
     case State of
         #{loop_pid := LoopPid} -> unlink(LoopPid), exit(LoopPid, shutdown);
@@ -69,6 +100,8 @@ rx_off(#{phy_layer := PhyModule} = State) ->
     PhyModule:write(sys_cfg, #{rxwtoe => 1}),
     {ok, maps:remove(loop_pid, State)}.
 
+% @doc clean up function to stop the mac layer
+% @end
 terminate(State, _Reason) -> 
     case State of
         #{loop_pid := LoopPid} -> unlink(LoopPid), exit(LoopPid, stop);
